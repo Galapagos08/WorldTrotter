0b03d2ecc286c7425944e3fdc5183d9a71928496
Dan Esrey <danesrey@dans-macbook-air.local>
2016-09-11 14:09:21 -0400

    finished lesson

--- WorldTrotter/ConversionViewController.h
+++ WorldTrotter/ConversionViewController.h
@@ -1,20 +1,15 @@
 //
-//  ViewController.m
+//  ViewController.h
 //  WorldTrotter
 //
 //  Created by Dan Esrey on 2016/11/09.
 //  Copyright © 2016 Dan Esrey. All rights reserved.
 //
 
-#import "ViewController.h"
+#import <UIKit/UIKit.h>
 
-@interface ViewController ()
-
-@end
-
-@implementation ViewController
-
-
+@interface ConversionViewController : UIViewController
 
 
 @end
+
---
+++ WorldTrotter/ConversionViewController.m
@@ -0,0 +1,65 @@
+//
+//  ViewController.m
+//  WorldTrotter
+//
+//  Created by Dan Esrey on 2016/11/09.
+//  Copyright © 2016 Dan Esrey. All rights reserved.
+//
+
+#import "ConversionViewController.h"
+
+@interface ConversionViewController ()
+
+@property (nonatomic) IBOutlet UILabel *celsiusLabel;
+@property (nonatomic) IBOutlet UITextField *fahrenheitField;
+@property (nonatomic) double fahrenheitValue;
+@property (nonatomic) double celsiusValue;
+
+@end
+
+@implementation ConversionViewController
+
+-(void)setFahrenheitValue:(double)fahrenheitValue {
+    _fahrenheitValue = fahrenheitValue;
+    [self updateCelsiusLabel];
+}
+
+-(void)setCelsiusValue:(double)celsiusValue {
+    self.fahrenheitValue = celsiusValue * (9.0/5.0) + 32;
+}
+
+-(double)celsiusValue {
+    return (self.fahrenheitValue - 32) * (5.0/9.0);
+}
+
+-(void)updateCelsiusLabel {
+    self.celsiusLabel.text = [self.numberFormatter stringFromNumber:@(self.celsiusValue)];
+}
+
+-(IBAction)fahrenheitFieldEditingChanged:(UITextField *)textField {
+    NSNumber *num = [self.numberFormatter numberFromString:textField.text];
+    if (num != nil) {
+        self.
+        fahrenheitValue = num.doubleValue;
+    } else {
+        self.celsiusLabel.text = @"???";
+    }
+}
+
+-(IBAction)dismissKeyboard:(id)sender {
+        [self.fahrenheitField resignFirstResponder];
+}
+
+- (NSNumberFormatter *)numberFormatter {
+    static NSNumberFormatter *formatter = nil;
+    if (formatter == nil) {
+        formatter = [NSNumberFormatter new];
+        formatter.numberStyle = NSNumberFormatterDecimalStyle;
+        formatter.minimumFractionDigits = 0;
+        formatter.maximumFractionDigits = 1;
+    }
+    return formatter;
+}
+
+
+@end
