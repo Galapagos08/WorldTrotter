adb7f8f5af6302d807da1dcca0a83466def5055d
Dan Esrey <danesrey@dans-macbook-air.local>
2016-09-11 12:35:27 -0400

    added constraints

--- WorldTrotter/ViewController.m
+++ WorldTrotter/ViewController.m
@@ -14,22 +14,7 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    
-    CGRect firstFrame = CGRectMake(160, 240, 100, 150);
-    UIView *firstView = [[UIView alloc] initWithFrame:firstFrame];
-    firstView.backgroundColor = [UIColor blueColor];
-    [self.view addSubview:firstView];
-                         
-    CGRect secondFrame = CGRectMake(20 , 30, 50, 50);
-    UIView *secondView = [[UIView alloc] initWithFrame:secondFrame];
-    secondView.backgroundColor = [UIColor greenColor];
-    [firstView addSubview:secondView];
-    
-    
 
-}
 
 
 @end
