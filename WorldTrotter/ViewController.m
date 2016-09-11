55246f5c282d8e2106e023afb22efa3444e03e99
Dan Esrey <danesrey@dans-macbook-air.local>
2016-09-11 11:37:44 -0400

    initial commit

--- WorldTrotter/ViewController.m
+++ WorldTrotter/ViewController.m
@@ -16,13 +16,19 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
+    
+    CGRect firstFrame = CGRectMake(160, 240, 100, 150);
+    UIView *firstView = [[UIView alloc] initWithFrame:firstFrame];
+    firstView.backgroundColor = [UIColor blueColor];
+    [self.view addSubview:firstView];
+                         
+    CGRect secondFrame = CGRectMake(20 , 30, 50, 50);
+    UIView *secondView = [[UIView alloc] initWithFrame:secondFrame];
+    secondView.backgroundColor = [UIColor greenColor];
+    [firstView addSubview:secondView];
+    
+    
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 
