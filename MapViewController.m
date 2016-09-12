03774bf175fc4f84589b58cfe2255852c83a1352
Dan Esrey <danesrey@dans-macbook-air.local>
2016-09-12 15:06:11 -0400

    added segmented control

--- MapViewController.m
+++ MapViewController.m
@@ -7,14 +7,66 @@
 //
 
 #import "MapViewController.h"
+#import <MapKit/MapKit.h>
 
 @interface MapViewController ()
+@property (nonatomic) MKMapView *mapView;
+
 
 @end
 
 @implementation MapViewController
 
 // MARK: - View lifecycle
+
+- (void)loadView {
+    // Create a map view
+    self.mapView = [MKMapView new];
+    // Set it as *the* view of the view controller
+    self.view = self.mapView;
+    
+    // Add a segmented control to the view
+    NSArray *segItems = @[ @"Standard", @"Hybrid", @"Satellite" ];
+    UISegmentedControl *segControl = [[UISegmentedControl alloc] initWithItems:segItems];
+    segControl.backgroundColor = [[UIColor whiteColor] colorWithAlphaComponent:0.5];
+    segControl.selectedSegmentIndex = 0;
+    
+    segControl.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addSubview:segControl];
+    
+    [segControl addTarget:self
+                   action:@selector(mapTypeChanged:)
+         forControlEvents:UIControlEventValueChanged];
+    
+    // Create and add Auto Layout constraints
+    NSLayoutConstraint *topConstraint =
+    [segControl.topAnchor constraintEqualToAnchor:self.topLayoutGuide.bottomAnchor constant:8];
+    UILayoutGuide *margins = self.view.layoutMarginsGuide;
+    NSLayoutConstraint *leadingConstraint = [segControl.leadingAnchor constraintEqualToAnchor:margins.leadingAnchor];
+    NSLayoutConstraint *trailingConstraint = [segControl.trailingAnchor constraintEqualToAnchor:margins.trailingAnchor];
+
+    topConstraint.active = YES;
+    leadingConstraint.active = YES;
+    trailingConstraint.active = YES;
+
+}
+
+// MARK: - Actions
+- (void)mapTypeChanged:(UISegmentedControl *)sender {
+    switch (sender.selectedSegmentIndex) {
+        case 0:
+            self.mapView.mapType = MKMapTypeStandard;
+            break;
+        case 1:
+            self.mapView.mapType = MKMapTypeHybrid;
+            break;
+        case 2:
+            self.mapView.mapType = MKMapTypeSatellite;
+        default:
+            break;
+    }
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     NSLog(@"MapViewController loaded its view.");
